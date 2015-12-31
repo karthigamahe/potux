37260f05deddb89ea98eae8fb98cc3f784a18bb9
devakdo <devakumar.mj@gmail.com>
2015-12-31 17:52:24 +0530

    basic crud for board

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'boards/new'
-
-  get 'boards/index'
-
-  get 'boards/show'
-
-  get 'boards/edit'
+  resources :boards
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
